726a2ea12116e6c891b233a30740e507033b584d
mdshahadad <social.shahadad@gmail.com>
2024-02-27 13:44:32 +0600

    Redesign the banner heigth

--- src/Home/Components/Banner/Banner.jsx
+++ src/Home/Components/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
     return (
-        <div className="bg-[#ECE8E5] h-fit overflow-x-hidden">
+        <div className="bg-[#ECE8E5] h-[600px] md:h-fit overflow-x-hidden">
             <div className="banner flex items-center justify-center md:px-20">
                 <div className="md:w-1/2 space-y-4 mx-8 md:mx-28 absolute md:static z-10 md:z-0 top-96 md:top-0">
                     <div className="">
@@ -11,8 +11,8 @@ const Banner = () => {
                         <button className="border-2 border-black px-8 py-2 rounded-md font-medium">Buy Now</button>
                     </div>
                 </div>
-                <div className="h-screen md:pt-20">
-                    <img className="w-[480px] md:h-[780px] md:w-[900px] mt-20 md:mt-0 scale-125 md:scale-100" src="https://i.postimg.cc/3J5kvZKp/banner-Chair-removebg.png" alt="" />
+                <div className="md:pt-24 ">
+                    <img className="w-[480px] md:w-full mt-20 md:mt-0 scale-125 md:scale-100" src="https://i.postimg.cc/3J5kvZKp/banner-Chair-removebg.png" alt="" />
                 </div>
             </div>
         </div>
